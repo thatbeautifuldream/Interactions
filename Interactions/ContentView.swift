dffbc506f772cca7a50eca971b522b4d9097653e
Milind Mishra <milind.mishra4@gmail.com>
2026-04-23 05:31:37 +0530

    feat: init paste toggle interaction withAnimation

---
+++ Interactions/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Interactions
+//
+//  Created by Milind Mishra on 23/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isToggled : Bool = false
+    var body: some View {
+        ZStack{
+            if isToggled {
+                Text("Paste")
+                    .font(.system(size: 16, design: .rounded))
+                    
+            } else {
+                Image(systemName: "arrow.right")
+                    .font(.system(size: 16, weight: .bold, design: .rounded))
+            }
+        }
+        .padding(.horizontal, 12)
+        .padding(.vertical, 8)
+        .background(.blue, in: Capsule(style: .continuous))
+        .foregroundStyle(.white)
+        .onTapGesture {
+            withAnimation(.snappy(duration: 0.36, extraBounce: 0.24)){
+                isToggled.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView().frame(width: 400, height: 400)
+}
---
+++ Interactions/InteractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractionsApp.swift
+//  Interactions
+//
+//  Created by Milind Mishra on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct InteractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
